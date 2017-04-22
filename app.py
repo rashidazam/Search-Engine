d4d58b3f93fd7701fd24de2e38de2d4bf8390ee0
Ankit Gaurav <ankitgaurav@outlook.com>
2017-04-23 00:30:16 +0530

    Add implementation of all sections of search engine

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
 from flask import Flask, render_template, request
-import searchengine
+import searchengine, neuralnet
 app = Flask(__name__)
+e = searchengine.searcher('searchengine.db')
+crawler=searchengine.crawler('searchengine.db')
+nnet = neuralnet.searchnet('nn.db')
 
 @app.route("/", methods=['POST', 'GET'])
 def search():
 	if request.method == 'POST':
 		queryText = request.form['q']
-		e = searchengine.searcher('searchengine.db')
-		list = e.query(queryText)
-		return render_template('index.html', list=list)
+		(wordids, urlIdsList, urlsList) = e.query(queryText)
+		listOfItems = []
+		for i in range(0, len(urlIdsList)):
+			listOfItems.append({'id': urlIdsList[i], 'url': urlsList[i]})
+		return render_template('index.html', list=listOfItems, q=queryText)
 	return render_template('index.html', list=None)
+
+@app.route('/train', methods=['POST', 'GET'])
+def train():
+	if request.method == 'POST':
+		queryPhrase = request.json['q']
+		selectedURLId = int(request.json['clicked'])
+		(wordids, urlIdsList, urlsList) = e.query(queryPhrase)
+		nnet.trainquery(wordids, urlIdsList, selectedURLId)
+		return 'done'
 
 @app.route("/crawl")
 def crawl():
 	pagelist=['https://en.wikipedia.org/wiki/Python_(programming_language)']
-	crawler=searchengine.crawler('searchengine.db')
 	crawler.crawl(pagelist)
-	return ('Hi')
+	return ('Crawling Complete')
 
 @app.route("/create")
 def createDB():
-	crawler=searchengine.crawler('searchengine.db')
 	crawler.createindextables()
 	return ('Created')
 
